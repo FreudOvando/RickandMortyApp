50de5d5c1b4a2b896924599bd20af78014dd1139
Freud Ovando Lara <freud5310@gmail.com>
2024-05-04 17:37:01 -0600

    i finish the alive or dead utility

--- src/App.jsx
+++ src/App.jsx
@@ -26,13 +26,17 @@ function App() {
     setLocationId(inputId.current.value.trim());
   };
 
+  const bg = {
+    backgroudImage: 'url(./assets/fondo1.jpeg)'
+  }
+
   return (
-    <div className="bg-stone-900 flex justify-center flex-col items-center h-auto w-auto min-w-[350px] ">
-      <div className="flex justify-center items-center mb-0  m-2 p-2 flex-col w-auto h-auto sm:text-justify text-wrap">
-        <h1 className="text-wrap text-justify font-mono font-bold text-3xl mt-2 mb-2 text-green-500 indent-1 tracking-tighter">
+    <div style={bg} className="bg-stone-900 flex justify-center flex-col items-center h-auto w-auto min-w-[350px] ">
+      <div className="flex justify-center items-center mb-0  m-2 p-2 flex-col w-auto h-auto">
+        <h1 className="flex-wrap text-wrap text-justify font-mono font-bold text-xl text-green-500">
           Rick and Morty Location App:
         </h1>
-        <h2 className="text-wrap text-justify font-mono font-bold text-3xl mt-2 mb-2 text-green-500 indent-1 tracking-tighter">
+        <h2 className="flex-wrap text-wrap text-justify font-mono font-bold text-xl mt-2 mb-2 text-green-500">
           Find your character by location
         </h2>
         <form
@@ -40,6 +44,7 @@ function App() {
           className=" flex justify-center items-center flex-row m-2 p-2 "
         >
           <input
+          id="input"
             placeholder={locationId}
             ref={inputId}
             type="number"
@@ -53,7 +58,18 @@ function App() {
 
       {
       hasError ? (
-        <h2> ✖ provide a correct id could be 1 or 126</h2>
+        <section class="flex items-center h-screen p-16 bg-gray-50 dark:bg-gray-700">
+    <div class="container flex flex-col items-center ">
+        <div class="flex flex-col gap-6 max-w-md text-center">
+            <h2 class="font-extrabold text-9xl text-gray-600 dark:text-gray-100">
+                <span class="sr-only">Error</span>
+            </h2>
+            <p class="text-2xl md:text-3xl dark:text-gray-300">Please, you must provide a number between 1 or 126.</p>
+            <a href="#" class="px-8 py-4 text-xl font-semibold rounded bg-green-600 text-gray-50 hover:text-gray-200">Try another number</a>
+        </div>
+    </div>
+</section>
+        
       ) : (
         <>
           <section className="flex justify-center items-center m-2 p-2 w-auto h-auto">
--- src/component/ResidentCard.jsx
+++ src/component/ResidentCard.jsx
@@ -18,12 +18,12 @@ const ResidentCard = ({url}) => {
   
     
     return ( 
-    <article className="min-w-[14rem] w-[20rem] h-auto m-2 mb-2 p-2 bg-slate-500 rounded-lg flex justify-center gap-2 items-center flex-col flex-wrap shadow-md shadow-green-500 max-sm:flex-col">
+    <article className="min-w-[18rem] max-w-[30rem] w-auto h-auto m-2 mb-2 p-2 bg-slate-500 rounded-lg flex justify-center gap-2 items-center  flex-wrap shadow-md shadow-green-500 max-sm:flex-col">
 <header className="relative">
-    <img src={resident?.image} alt="" className="w-[15rem] h-[15rem] " />
-    <div className="absolute top-0 left-0 z-10 ml-2 mt-2 bg-blue-500 bg-opacity-50 w-[6rem] h-[2rem] flex justify-center items-center ">
-        <div className=" w-3 h-3 bg-red-500 bg-opacity-80 rounded-full flex flex-row justify-center items-center mr-2"></div>
-        <div className="flex justify-end items-end text-xl font-mono text-green-500">{resident?.status}</div>
+    <img src={resident?.image} alt="" className="w-[15rem] h-[15rem] overflow-hidden rounded-lg shadow-lg shadow-black " />
+    <div className="absolute top-0 left-0 z-10 ml-2 mt-2 bg-blue-500 bg-opacity-50 w-[7.5rem] h-[2.2rem] flex justify-center items-center ">
+    <div id="status" className={` w-3 h-3 rounded-full flex flex-row justify-center items-center mr-2 ${resident?.status === 'Alive' ? 'bg-green-500' : (resident?.status === 'Dead' ? 'bg-red-500' : 'bg-yellow-500')}`}></div>
+        <div id="resident-status" className="flex justify-end items-end text-xl font-mono text-green-500">{resident?.status}</div>
     </div>
 </header>
 
@@ -31,9 +31,9 @@ const ResidentCard = ({url}) => {
     <h3 className="text-2xl text-wrap text-center text-green-500"> {resident?.name} </h3>
     <hr className="" />
     <ul className="flex flex-wrap flex-col m-2 p-2">
-        <li className="font-mono text-justify text-xl text-wrap "><span>Specie:</span> {resident?.species} </li>
-        <li className="font-mono text-justify text-xl text-wrap"><span>Origin:</span> {resident?.origin.name} </li>
-        <li className="font-mono text-justify text-xl text-wrap"><span>specie:</span> {resident?.episode.length} </li>
+        <li className="font-mono text-justify text-2xl text-wrap p-1  "><span>Specie:</span> {resident?.species} </li>
+        <li className="font-mono text-justify text-2xl text-wrap p-1 flex-col flex"><span>Origin:</span> {resident?.origin.name} </li>
+        <li className="font-mono text-justify text-2xl text-wrap p-1"><span>specie:</span> {resident?.episode.length} </li>
        
     </ul>
 </section>
